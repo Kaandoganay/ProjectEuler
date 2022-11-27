edcb99d17479eb24fa9f6bd2e717bdf72fb68686
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2022-11-27 18:53:44 +0300

    Update Problem2.hs

--- Problem2.hs
+++ Problem2.hs
@@ -1,12 +1,5 @@
 
-
-
-fibon :: (Eq t, Num t, Num a) => t -> [a]
-fibon 1 = [1]
-fibon 2 = [1,1]
-fibon 3 = fibon 2 ++ [last (fibon 2) + last (fibon 1)]
-fibon n = fibon (n-1) ++ [last (fibon (n-1)) + last (fibon (n-2))]
-
-smallfibon :: [a]
-smallfibon = fibon n 
-
+solution2 :: Integer
+solution2 = sum [ x | x <- takeWhile (<= 4000000) fibs, even x]
+  where
+    fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
