70e67c7476fec61602a8d9b97084c87747878ac3
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2022-12-05 14:57:05 +0300

    ProjectEuler

---
+++ problem10.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+primes :: [Integer]
+primes = 2 : filter (null . tail . primeFactors) [3,5..]
+
+primeFactors :: Integer -- ^ 
+  -> [Integer]
+primeFactors n = factor n primes
+  where
+    factor n (p:ps) 
+        | p*p > n        = [n]
+        | n `mod` p == 0 = p : factor (n `div` p) (p:ps)
+        | otherwise      =     factor n ps
+
+
+
+sumprimes :: Integer
+sumprimes = sum (takeWhile (<2000000) primes)
