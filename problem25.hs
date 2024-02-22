97e5a999e6edb3fd137172694928838b4a368bb4
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-02-22 15:12:47 +0100

    Create problem25.hs

---
+++ problem25.hs
@@ -0,0 +1,18 @@
+fibs :: [Integer]
+fibs = 1:1:zipWith (+) fibs (tail fibs)
+
+fib :: Int -> Integer
+fib n = fibs !!(n-1)
+
+makeInteger :: [String]
+makeInteger = map show fibs
+
+firstInteger :: Integer
+firstInteger = read $ head [x | x <- makeInteger, length x == 1000]
+
+indexInteger :: String
+indexInteger = show $ length $ takeWhile (<= firstInteger) fibs
+
+main :: IO ()
+main = do
+    putStr indexInteger
