4f39fee65ff972ad0daae6f13ba7f0b6d39915eb
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-02-27 23:15:43 +0100

    Update problem10.hs

--- problem10.hs
+++ problem10.hs
@@ -1,15 +1,13 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 primes :: [Integer]
 primes = 2 : filter (null . tail . primeFactors) [3,5..]
 
-primeFactors :: Integer -- ^ 
-  -> [Integer]
+primeFactors :: Integer -> [Integer]
 primeFactors n = factor n primes
   where
     factor n (p:ps) 
         | p*p > n        = [n]
         | n `mod` p == 0 = p : factor (n `div` p) (p:ps)
-        | otherwise      =     factor n ps
+        | otherwise = factor n ps
 
 
 
