e580f097358b4ecddc24c45a89ca746ac8be84b8
Kaan Doğanay <kaan.doganayy@gmail.com>
2024-03-06 05:01:28 +0100

    Update problem38.hs

--- problem38.hs
+++ problem38.hs
@@ -1,7 +1,23 @@
 import Data.List
+import Data.Char(intToDigit)
 
 pandigital :: [String]
 pandigital = permutations "123456789"
 
+
 multiple :: [[Integer]]
-multiple = [map (*n) [1..9]| n <- [1..999]]
+multiple =[map (*n) [1..k]| n <- [1..10000], k <- [1,2,3]]
+
+joiner :: [Integer] -> String
+joiner = concatMap show
+
+
+concatMultiple :: [String]
+concatMultiple = filter (`elem` pandigital) (map joiner multiple)
+
+makeInt :: Read Int => [String] -> [Int]
+makeInt = map read
+
+main :: IO ()
+main = do
+  print $ maximum ( makeInt concatMultiple)
