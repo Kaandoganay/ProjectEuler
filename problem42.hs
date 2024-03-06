40c6915b4c496016072279662a13123d9e0a4144
Kaan Doğanay <kaan.doganayy@gmail.com>
2024-03-06 01:56:14 +0100

    Update problem42.hs

--- problem42.hs
+++ problem42.hs
@@ -3,7 +3,7 @@ import System.IO
 
 import Data.List
 
-triangle :: [Integer]
+triangle :: [Int]
 triangle = [(x * (x+1)) `div` 2| x <-[1..2000]]
 
 order :: Char -> Int
@@ -15,3 +15,11 @@ nameScores = map (map order)
 sumScores :: [[Char]] -> [Int]
 sumScores xs = map sum (nameScores xs)
 
+set :: [[Char]] -> [Int]
+set xs = filter (`elem` triangle) (sumScores xs)
+
+main :: IO ()
+main= do
+  name <- readFile "0042_words.txt"
+  let names = read ("["++ name++"]")
+  print (length (set names))
