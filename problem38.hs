8b90f0664d28f8c31ec5804384e1a014be34d483
Kaan Doğanay <kaan.doganayy@gmail.com>
2024-03-06 05:05:54 +0100

    Update problem38.hs

--- problem38.hs
+++ problem38.hs
@@ -3,13 +3,11 @@ import Data.List
 pandigital :: [String]
 pandigital = permutations "123456789"
 
-
 multiple :: [[Integer]]
 multiple =[map (*n) [1..k]| n <- [1..10000], k <- [1,2,3]]
 
 joiner :: [Integer] -> String
 joiner = concatMap show
-
 
 concatMultiple :: [String]
 concatMultiple = filter (`elem` pandigital) (map joiner multiple)
