80c67261775fa0e35851c0b6f21633bca8dad702
Kaan Doğanay <kaan.doganayy@gmail.com>
2024-03-06 17:03:04 +0100

    Update problem32.hs

--- problem32.hs
+++ problem32.hs
@@ -1,11 +1,24 @@
 import Data.List
+import Control.Monad
 
-pandigital :: [String]
-pandigital = permutations "123456789"
+combs 0 xs = [([],xs)]
+combs n xs = [(y:ys,rest) | y <- xs, (ys,rest) <- combs (n-1) (delete y xs)]
 
-pandigitalProducts xs = concatMap show xs `elem` pandigital
+l2n :: (Integral a) => [a] -> a
+l2n = foldl' (\a b -> 10*a+b) 0
 
-set :: [[Integer]]
-set = [ [x,y,z]| x <- [1..1000], y <- [1..1000], let z = x*y,pandigitalProducts [x,y,z]]
+swap (a,b) = (b,a)
 
+explode :: (Integral a) => a -> [a]
+explode = unfoldr (\a -> if a==0 then Nothing else Just . swap $ quotRem a 10)
 
+pandigiticals =
+  nub $ do (beg,end) <- combs 5 [1..9]
+           n <- [1,2]
+           let (a,b) = splitAt n beg
+               res = l2n a * l2n b
+           guard $ sort (explode res) == end
+           return res
+
+problem_32 = do 
+    print $ sum pandigiticals
