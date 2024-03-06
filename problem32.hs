34858f0e6b304083592d32d34aaa1ee1263faeb3
Kaan Doğanay <kaan.doganayy@gmail.com>
2024-03-06 17:03:32 +0100

    Update problem32.hs

--- problem32.hs
+++ problem32.hs
@@ -1,17 +1,20 @@
 import Data.List
 import Control.Monad
 
+combs :: (Num t, Eq t, Eq a) => t -> [a] -> [([a], [a])]
 combs 0 xs = [([],xs)]
 combs n xs = [(y:ys,rest) | y <- xs, (ys,rest) <- combs (n-1) (delete y xs)]
 
 l2n :: (Integral a) => [a] -> a
 l2n = foldl' (\a b -> 10*a+b) 0
 
+swap :: (b, a) -> (a, b)
 swap (a,b) = (b,a)
 
 explode :: (Integral a) => a -> [a]
 explode = unfoldr (\a -> if a==0 then Nothing else Just . swap $ quotRem a 10)
 
+pandigiticals :: [Integer]
 pandigiticals =
   nub $ do (beg,end) <- combs 5 [1..9]
            n <- [1,2]
@@ -20,5 +23,6 @@ pandigiticals =
            guard $ sort (explode res) == end
            return res
 
-problem_32 = do 
+main :: IO ()
+main= do 
     print $ sum pandigiticals
