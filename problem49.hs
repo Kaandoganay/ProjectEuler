7576b94cc7ba1b669533af9f978e38a118fdc149
Kaan Doğanay <kaan.doganayy@gmail.com>
2024-03-06 15:39:34 +0100

    Create problem49.hs

---
+++ problem49.hs
@@ -0,0 +1,15 @@
+import Data.Numbers.Primes
+import Data.List
+
+fourDigitPrimes :: [Integer]
+fourDigitPrimes = filter isPrime [1000..9999]
+
+thePrimes :: [(Integer, Integer, Integer)]
+thePrimes = [[x, y, z] |
+               x <- fourDigitPrimes,
+               y <- filter (> x) fourDigitPrimes,
+               sort (show x) == sort (show y),
+               z <- filter (> y) fourDigitPrimes,
+               sort (show y) == sort (show z),
+                z == 2 * y - x]
+
