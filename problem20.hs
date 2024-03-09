9ca164004ed0896eb91396e39d129b7131e87765
Kaan Doğanay <kaan.doganayy@gmail.com>
2024-03-09 15:53:56 +0100

    Update problem20.hs

--- problem20.hs
+++ problem20.hs
@@ -1,14 +1,10 @@
-import Basement.IntegralConv
+import Data.Char
 
 factorial :: (Show a, Num a, Enum a) => a -> String
 factorial n = show (product [1..n])
 
-digitSum :: [Char] -> [Int]
-digitSum "" = [0]
-digitSum xs = (charToInt (head xs) - 48) : digitSum (tail xs)
-
 factorialDigitSum :: (Show a, Num a, Enum a) => a -> Int
-factorialDigitSum n = sum $ digitSum (factorial n)
+factorialDigitSum n  = sum $ map digitToInt (factorial n)
 
 main :: IO ()
 main = do
